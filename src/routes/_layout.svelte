d68ab7efbf206f562affc58d778572882508a046
Ollie Taylor <contact@olliejt.com>
2020-05-19 18:30:12 +0200

    init

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import LogRocket from 'logrocket';
+	LogRocket.init('TODO');
+
+	import Nav from '../components/Nav.svelte';
+
+	export let segment;
+</script>
+
+<style global lang="scss">
+	@import '../styles/main.scss';
+</style>
+
+<Nav {segment} />
+
+<main>
+	<slot />
+</main>
