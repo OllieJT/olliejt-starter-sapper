de1ceb66826a5300ff8034ba12b341a42423609c
Ollie Taylor <contact@olliejt.com>
2020-05-26 17:47:23 +0200

    Working Queries

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,38 @@
+<script context="module">
+	import client from '../data/apollo';
+	import { gql } from 'apollo-boost';
+
+	const EVERYTHING = gql`
+		query shop {
+			shop {
+				name
+			}
+		}
+	`;
+	export async function preload() {
+		return {
+			cache: await client.query({
+				query: EVERYTHING,
+			}),
+		};
+	}
+</script>
+
+<script>
+	import { setClient, restore, query } from 'svelte-apollo';
+	export let cache;
+	restore(client, EVERYTHING, cache.data);
+	// TODO Uncommenting this part triggers a 500 error.
+	// setClient(client);
+	// query a subset of the preloaded (the rest if for Account)
+	const todos = query(client, { query: EVERYTHING });
+	console.log(todos);
+</script>
+
 <style>
-	h1, figure, p {
+	h1,
+	figure,
+	p {
 		text-align: center;
 		margin: 0 auto;
 	}
@@ -36,11 +69,23 @@
 	<title>Sapper project template</title>
 </svelte:head>
 
+{#await $todos}
+	<p>Loading...</p>
+{:then result}
+	{#if result.data}
+		<ul>{JSON.stringify(result.data)}</ul>
+	{:else}
+		<p>ERROR!!</p>
+	{/if}
+{/await}
+
 <h1>Great success!</h1>
 
 <figure>
-	<img alt='Success Kid' src='successkid.jpg'>
+	<img alt="Success Kid" src="successkid.jpg" />
 	<figcaption>Have fun with Sapper!</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p>
+	<strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong>
+</p>
