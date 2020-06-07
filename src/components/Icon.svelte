0abb2e7b4ee3045bb18a2d5bcfc2ad1cf9130734
Ollie Taylor <contact@olliejt.com>
2020-06-07 16:55:51 +0200

    SVG Icons

---
+++ src/components/Icon.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let name;
+	export let title;
+	export let desc = undefined;
+</script>
+
+<svg class="svg-icon">
+	{#if title}
+		<title>{title}</title>
+	{:else}
+		<title>{name}</title>
+	{/if}
+	{#if desc}
+		<desc>{desc}</desc>
+	{/if}
+	<use xlink:href="/icons.svg#icon-{name}" />
+</svg>
